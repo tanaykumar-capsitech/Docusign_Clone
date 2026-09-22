43bef257d2c3ffe016ba6a65d4cf4d13a44e2997
Tanay Kumar <tanay.kumar@capsitech.com>
2026-09-22 20:00:05 +0530

    Created basic folder structure and updated backend settings

---
+++ backend/backend/Settings/DatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Settings
+{
+    public class DatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string ServiceEntriesCollection { get; set; }
+    }
+}
